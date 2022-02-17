32716c7cfb40af43c45dc4089fc2b2e7fc6a34d9
neeraj15022001 <gneeraj32595@gmail.com>
2022-02-17 16:38:19 +0530

    Register Page Done

--- src/Components/Pages/Register/Register.jsx
+++ src/Components/Pages/Register/Register.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Form from "../../Shared/Form/Form";
 
 function Register() {
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+        <section className={"flex h-screen"}>
+            <section className={"flex-1 flex flex-col justify-center px-5 box-border"}>
+                <div className={"mb-8"}>
+                    <p className={"text-5xl font-bold capitalize ml-3 text-gray-600"}>create account</p>
+                </div>
+                <Form loginForm={false} />
+            </section>
+            <section className={"flex-1 bg-blue-100 box-border"}></section>
+        </section>
     );
 }
 
--- src/Components/Shared/AppBar/AppBar.jsx
+++ src/Components/Shared/AppBar/AppBar.jsx
@@ -3,21 +3,29 @@ import {Link} from "react-router-dom";
 
 function AppBar() {
     return (
-        <nav
-            className={"flex items-center px-5 py-4 bg-white fixed right-0 left-0 top-0 backdrop-filter backdrop-blur-xl bg-opacity-30 border border-gray-200"}>
+        <>
+            <nav
+                className={"flex items-center px-5 py-4 bg-white fixed right-0 left-0 top-0 backdrop-filter backdrop-blur-xl bg-opacity-30 border border-gray-200"}>
             <span className={"uppercase text-gray-600 font-bold cursor-pointer text-3xl"}><Link to={"/"}><span
                 className={"uppercase text-blue-600"}>med</span>icos</Link></span>
-            <div className={"flex-1 flex items-center justify-end"}>
-                <button
-                    className={"bg-blue-600 transition hover:bg-blue-700 px-8 py-2 capitalize text-white rounded-full mr-2"}>
-                    <Link to={"/login"}>login</Link>
-                </button>
-                <button
-                    className={"bg-gray-400 transition hover:bg-gray-500 px-8 py-2 capitalize text-white rounded-full"}>
-                    <Link to={"/register"}>register</Link>
-                </button>
-            </div>
-        </nav>
+                <div className={"flex-1 flex items-center justify-end"}>
+                    <button
+                        className={"bg-blue-600 transition hover:bg-blue-700 px-8 py-2 capitalize text-white rounded-full mr-2"}>
+                        <Link to={"/login"}>login</Link>
+                    </button>
+                    <button
+                        className={"bg-gray-400 transition hover:bg-gray-500 px-8 py-2 capitalize text-white rounded-full"}>
+                        <Link to={"/register"}>register</Link>
+                    </button>
+                </div>
+            </nav>
+            <section className={"flex h-12"}>
+                <section className={"flex-1 flex flex-col justify-center px-5 box-border"}></section>
+                <section className={"flex-1 bg-blue-100 box-border"}></section>
+            </section>
+        </>
+
+
     );
 }
 
