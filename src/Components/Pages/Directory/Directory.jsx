ad3a5021e2f54c05d6bbb3ca3d5afa1c597a4850
neeraj15022001 <gneeraj32595@gmail.com>
2022-02-17 19:52:41 +0530

    drectory page UI done

--- src/Components/Pages/Directory/Directory.jsx
+++ src/Components/Pages/Directory/Directory.jsx
@@ -1,9 +1,84 @@
 import React from 'react';
+import pill from "../../../assets/pill.png";
 
 function Directory() {
     return (
-        <section className={"p-5"}>
-            <h1>Directory</h1>
+        <section className={"p-10"}>
+            <div className="flex flex-col">
+                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table className="min-w-full divide-y divide-gray-200">
+                                <thead className="bg-blue-600">
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                                    >
+                                        Name
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                                    >
+                                        brand
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                                    >
+                                        price
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                                    >
+                                        availability
+                                    </th>
+                                    <th scope="col" className="relative px-6 py-3">
+                                        <span className="sr-only">Edit</span>
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody className="bg-white divide-y divide-gray-200">
+                                <tr>
+                                    <td className="px-6 py-4 whitespace-nowrap">
+                                        <div className="flex items-center">
+                                            <div className="flex-shrink-0 h-10 w-10">
+                                                <img className="h-10 w-10 rounded-full" src={pill} alt=""/>
+                                            </div>
+                                            <div className="ml-4">
+                                                <div
+                                                    className="text-sm font-medium text-gray-900 capitalize">glimisave
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap">
+                                        <div className="text-sm text-gray-900 capitalize">candid</div>
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 capitalize">Rs
+                                        120
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap">
+                          <span
+                              className=" capitalize px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                            yes
+                          </span>
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                        <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                                            More Details
+                                        </a>
+                                    </td>
+                                </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
